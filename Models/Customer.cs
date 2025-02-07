8052fe85fb2f48382b067f113584090290f06779
Garrett Smith <gcsmith.dev@gmail.com>
2025-02-07 15:06:34 -0600

    initial commit

---
+++ Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using DesktopSchedulingApp.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSchedulingApp.Models
+{
+    public class Customer : Address
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public bool Active { get; set; }
+
+        public Customer(){ }
+
+        // AddCustomer constructor
+        public Customer(string customerName, string address, string postalCode, string phone, string city, string country, bool active)
+        {
+            CustomerId = CustomerService.GetNewCustomerID();
+            CustomerName = customerName;
+            AddressId = AddressService.GetNewAddressID();
+            StreetAddress = address;
+            PostalCode = postalCode;
+            Phone = phone;
+            CityName = city;
+            CountryName = country;
+            Active = active;
+        }
+
+        //customerID, name, active, address, postal code, phone, city, county
+        public Customer(int customerId, string customerName, int addressId, string address, string postalCode, string phone, string city, string country, bool active)
+        {
+            CustomerId = customerId;
+            CustomerName = customerName;
+            AddressId = addressId;
+            StreetAddress = address;
+            PostalCode = postalCode;
+            Phone = phone;
+            CityName = city;
+            CountryName = country;
+            Active = active;
+        }
+    }
+}
