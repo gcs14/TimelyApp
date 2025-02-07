919e6053189ef9d0a4117b51177e24212ee8396a
Garrett Smith <gcsmith.dev@gmail.com>
2025-02-07 15:06:21 -0600

    initial commit

---
+++ Models/City.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSchedulingApp.Models
+{
+    public class City : Country
+    {
+        public int CityId {  get; set; }
+        public string CityName {  get; set; }
+
+        public City() { }
+
+        public City(int cityId, string city, int countryId)
+        {
+            this.CityId = cityId;
+            this.CityName = city;
+            this.CountryId = countryId;
+        }
+    }
+}
