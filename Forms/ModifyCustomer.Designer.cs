35f086c538251392a8c5b241523d0798c31f705e
Garrett Smith <gcsmith.dev@gmail.com>
2025-02-07 15:05:27 -0600

    update customer entity to access and modify data from Country, City, and Address tables

---
+++ Forms/ModifyCustomer.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace DesktopSchedulingApp.Forms
+{
+    partial class ModifyCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            addCustomerLabel = new System.Windows.Forms.Label();
+            customerNameLabel = new System.Windows.Forms.Label();
+            customerNameText = new System.Windows.Forms.TextBox();
+            phoneLabel = new System.Windows.Forms.Label();
+            phoneText = new System.Windows.Forms.TextBox();
+            cityLabel = new System.Windows.Forms.Label();
+            postalLabel = new System.Windows.Forms.Label();
+            cityText = new System.Windows.Forms.TextBox();
+            postalText = new System.Windows.Forms.TextBox();
+            countryLabel = new System.Windows.Forms.Label();
+            countryComboBox = new System.Windows.Forms.ComboBox();
+            activeCheckBtn = new System.Windows.Forms.CheckBox();
+            modifyCustomerSubmitBtn = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // addCustomerLabel
+            // 
+            addCustomerLabel.AutoSize = true;
+            addCustomerLabel.Font = new System.Drawing.Font("Arial Black", 27F);
+            addCustomerLabel.ForeColor = System.Drawing.Color.MediumPurple;
+            addCustomerLabel.Location = new System.Drawing.Point(0, 9);
+            addCustomerLabel.Name = "addCustomerLabel";
+            addCustomerLabel.Size = new System.Drawing.Size(419, 51);
+            addCustomerLabel.TabIndex = 1;
+            addCustomerLabel.Text = "MODIFY CUSTOMER";
+            // 
+            // customerNameLabel
+            // 
+            customerNameLabel.AutoSize = true;
+            customerNameLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            customerNameLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            customerNameLabel.Location = new System.Drawing.Point(27, 85);
+            customerNameLabel.Name = "customerNameLabel";
+            customerNameLabel.Size = new System.Drawing.Size(49, 20);
+            customerNameLabel.TabIndex = 3;
+            customerNameLabel.Text = "Name";
+            // 
+            // customerNameText
+            // 
+            customerNameText.Location = new System.Drawing.Point(27, 108);
+            customerNameText.Name = "customerNameText";
+            customerNameText.Size = new System.Drawing.Size(170, 23);
+            customerNameText.TabIndex = 4;
+            // 
+            // phoneLabel
+            // 
+            phoneLabel.AutoSize = true;
+            phoneLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            phoneLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            phoneLabel.Location = new System.Drawing.Point(27, 143);
+            phoneLabel.Name = "phoneLabel";
+            phoneLabel.Size = new System.Drawing.Size(108, 20);
+            phoneLabel.TabIndex = 5;
+            phoneLabel.Text = "Phone Number";
+            // 
+            // phoneText
+            // 
+            phoneText.Location = new System.Drawing.Point(27, 170);
+            phoneText.Name = "phoneText";
+            phoneText.Size = new System.Drawing.Size(170, 23);
+            phoneText.TabIndex = 6;
+            // 
+            // cityLabel
+            // 
+            cityLabel.AutoSize = true;
+            cityLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            cityLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            cityLabel.Location = new System.Drawing.Point(27, 212);
+            cityLabel.Name = "cityLabel";
+            cityLabel.Size = new System.Drawing.Size(34, 20);
+            cityLabel.TabIndex = 7;
+            cityLabel.Text = "City";
+            // 
+            // postalLabel
+            // 
+            postalLabel.AutoSize = true;
+            postalLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            postalLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            postalLabel.Location = new System.Drawing.Point(227, 212);
+            postalLabel.Name = "postalLabel";
+            postalLabel.Size = new System.Drawing.Size(87, 20);
+            postalLabel.TabIndex = 8;
+            postalLabel.Text = "Postal Code";
+            // 
+            // cityText
+            // 
+            cityText.Location = new System.Drawing.Point(27, 235);
+            cityText.Name = "cityText";
+            cityText.Size = new System.Drawing.Size(170, 23);
+            cityText.TabIndex = 9;
+            // 
+            // postalText
+            // 
+            postalText.Location = new System.Drawing.Point(227, 235);
+            postalText.Name = "postalText";
+            postalText.Size = new System.Drawing.Size(170, 23);
+            postalText.TabIndex = 10;
+            // 
+            // countryLabel
+            // 
+            countryLabel.AutoSize = true;
+            countryLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            countryLabel.ForeColor = System.Drawing.SystemColors.HighlightText;
+            countryLabel.Location = new System.Drawing.Point(27, 275);
+            countryLabel.Name = "countryLabel";
+            countryLabel.Size = new System.Drawing.Size(60, 20);
+            countryLabel.TabIndex = 11;
+            countryLabel.Text = "Country";
+            // 
+            // countryComboBox
+            // 
+            countryComboBox.FormattingEnabled = true;
+            countryComboBox.Location = new System.Drawing.Point(27, 298);
+            countryComboBox.Name = "countryComboBox";
+            countryComboBox.Size = new System.Drawing.Size(121, 23);
+            countryComboBox.TabIndex = 12;
+            // 
+            // activeCheckBtn
+            // 
+            activeCheckBtn.AutoSize = true;
+            activeCheckBtn.Font = new System.Drawing.Font("Segoe UI", 11F);
+            activeCheckBtn.ForeColor = System.Drawing.SystemColors.HighlightText;
+            activeCheckBtn.Location = new System.Drawing.Point(232, 295);
+            activeCheckBtn.Name = "activeCheckBtn";
+            activeCheckBtn.Size = new System.Drawing.Size(69, 24);
+            activeCheckBtn.TabIndex = 13;
+            activeCheckBtn.Text = "Active";
+            activeCheckBtn.UseVisualStyleBackColor = true;
+            // 
+            // modifyCustomerSubmitBtn
+            // 
+            modifyCustomerSubmitBtn.AutoSize = true;
+            modifyCustomerSubmitBtn.BackColor = System.Drawing.Color.Silver;
+            modifyCustomerSubmitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            modifyCustomerSubmitBtn.Font = new System.Drawing.Font("Arial", 11F);
+            modifyCustomerSubmitBtn.Location = new System.Drawing.Point(165, 347);
+            modifyCustomerSubmitBtn.Name = "modifyCustomerSubmitBtn";
+            modifyCustomerSubmitBtn.Size = new System.Drawing.Size(75, 32);
+            modifyCustomerSubmitBtn.TabIndex = 14;
+            modifyCustomerSubmitBtn.Text = "Submit";
+            modifyCustomerSubmitBtn.UseVisualStyleBackColor = false;
+            modifyCustomerSubmitBtn.Click += ModifyCustomerSubmitBtn_Click;
+            // 
+            // ModifyCustomer
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.Color.FromArgb(18, 18, 18);
+            ClientSize = new System.Drawing.Size(418, 412);
+            Controls.Add(modifyCustomerSubmitBtn);
+            Controls.Add(activeCheckBtn);
+            Controls.Add(countryComboBox);
+            Controls.Add(countryLabel);
+            Controls.Add(postalText);
+            Controls.Add(cityText);
+            Controls.Add(postalLabel);
+            Controls.Add(cityLabel);
+            Controls.Add(phoneText);
+            Controls.Add(phoneLabel);
+            Controls.Add(customerNameText);
+            Controls.Add(customerNameLabel);
+            Controls.Add(addCustomerLabel);
+            Name = "ModifyCustomer";
+            Text = "ModifyCustomer";
+            Load += ModifyCustomer_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label addCustomerLabel;
+        private System.Windows.Forms.Label customerNameLabel;
+        private System.Windows.Forms.TextBox customerNameText;
+        private System.Windows.Forms.Label phoneLabel;
+        private System.Windows.Forms.TextBox phoneText;
+        private System.Windows.Forms.Label cityLabel;
+        private System.Windows.Forms.Label postalLabel;
+        private System.Windows.Forms.TextBox cityText;
+        private System.Windows.Forms.TextBox postalText;
+        private System.Windows.Forms.Label countryLabel;
+        private System.Windows.Forms.ComboBox countryComboBox;
+        private System.Windows.Forms.CheckBox activeCheckBtn;
+        private System.Windows.Forms.Button modifyCustomerSubmitBtn;
+    }
+}
---
+++ Forms/ModifyCustomer.cs
@@ -0,0 +1,44 @@
+﻿using DesktopSchedulingApp.Models;
+using DesktopSchedulingApp.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopSchedulingApp.Forms
+{
+    public partial class ModifyCustomer : Form
+    {
+        Customer currentCustomer;
+        string currentUserName;
+        public ModifyCustomer(string n, Customer c)
+        {
+            InitializeComponent();
+            currentCustomer = c;
+            currentUserName = n;
+        }
+
+        private void ModifyCustomer_Load(object sender, EventArgs e)
+        {
+            customerNameText.Text = currentCustomer.CustomerName;
+            activeCheckBtn.Checked = currentCustomer.Active;
+        }
+
+        private void ModifyCustomerSubmitBtn_Click(object sender, EventArgs e)
+        {
+            //CustomerService.ModifyCustomer(currentCustomer, new Customer(
+            //    currentCustomer.CustomerId,
+            //    customerNameText.Text,
+            //    4,
+            //    activeCheckBtn.Checked
+            //    )
+            //);
+            this.Close();
+        }
+    }
+}
